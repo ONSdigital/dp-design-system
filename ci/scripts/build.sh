0c1b7233ba8f77b6b46f82772ac4fa1ebf777709
Daniel W <danieldwalford@gmail.com>
2021-01-25 10:49:08 +0000

    Versioning managed in build via the build.sh
    This is instead of handling in the pipeline

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -3,6 +3,7 @@
 pushd dp-design-system
   npm install --unsafe-perm
   npm run build
+  SHORT_REF=`git rev-parse --short HEAD`
 popd
 
-cp -r dp-design-system/dist/css/main.css build/
+cp -r dp-design-system/dist/css/main.css build/$SHORT_REF
