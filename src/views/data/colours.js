a86a9ecfc0f810a91cda2ea039fab6e7c80dad90
Ben Armstrong <benarmstrong@BenAs-MacBook-Pro.local>
2021-02-24 10:46:15 +0000

    Addition of tints for reference

--- src/views/data/colours.js
+++ src/views/data/colours.js
@@ -27,4 +27,49 @@ module.exports = {
     'grey-5': '#F5F5F6',
     white: '#FFFFFF',
   },
+  tintsgeneral: {
+      'spring-green-70': '#c2d175', // RGB: (194, 209, 117)
+      'spring-green-40': '#dce5b0', // RGB: (220, 229, 176)
+      'spring-green-10': '#f6f8eb', // RGB: (246, 248, 235)
+      'aqua-teal-70': '#4dbfc1', // RGB: (77, 191, 193)
+      'aqua-teal-40': '#99dadb', // RGB: (153, 218, 219)
+      'aqua-teal-10': '#e6f6f6', // RGB: (230, 246, 246)
+      'sky-blue-70': '#68bddb', // RGB: (104, 189, 219)
+      'sky-blue-40': '#a9d9eb', // RGB: (169, 217, 235)
+      'sky-blue-10': '#e9f6fa', // RGB: (233, 246, 250)
+      'ocean-blue-70': '#6390b5', // RGB: (99, 144, 181)
+      'ocean-blue-40': '#a6bfd5', // RGB: (166, 191, 213)
+      'ocean-blue-10': '#e9eff4', // RGB: (233, 239, 244)
+      'night-blue-70': '#4d7789', // RGB: (77, 119, 137)
+      'night-blue-40': '#99b1bc', // RGB: (153, 177, 188)
+      'night-blue-10': '#e6ecee', // RGB: (230, 236, 23
+    },
+  tintscensus: {
+    'indigo-blue-70': '#7774b0', // RGB: (119, 116, 176)
+    'indigo-blue-40': '#b1afd2', // RGB: (177, 175, 210)
+    'indigo-blue-10': '#ecebf4', // RGB: (236, 235, 244)
+    'plum-purple-70': '#b163a8', // RGB: (177, 99, 168)
+    'plum-purple-40': '#d3a6cd', // RGB: (211, 166, 205)
+    'plum-purple-10': '#f4e9f3', // RGB: (244, 233, 243)
+    'flamingo-pink-70': '#e95195', // RGB: (233, 81, 149)
+    'flamingo-pink-40': '#f29bc2', // RGB: (242, 155, 194)
+    'flamingo-pink-10': '#fce6f0', // RGB: (252, 230, 240)
+  },
+  tintssupporting: {
+    'ruby-red-70': '#de4e5f', // RGB: (222, 78, 95)
+    'ruby-red-40': '#ec9aa4', // RGB: (236, 154, 164)
+    'ruby-red-10': '#fae6e8', // RGB: (250, 230, 232)
+    'jaffa-orange-70': '#fea162', // RGB: (254, 161, 98)
+    'jaffa-orange-40': '#ffc9a5', // RGB: (255, 201, 165)
+    'jaffa-orange-10': '#fff2e9', // RGB: (255, 242, 233)
+    'sun-yellow-70': '#fcd94d', // RGB: (252, 217, 77)
+    'sun-yellow-40': '#fde999', // RGB: (253, 233, 153)
+    'sun-yellow-10': '#fffae6', // RGB: (255, 250, 230)
+    'neon-yellow-70': '#f5f991', // RGB: (245, 249, 145)
+    'neon-yellow-40': '#f9fcc0', // RGB: (249, 252, 192)
+    'neon-yellow-10': '#fefeef', // RGB: (254, 254, 239)
+    'leaf-green-70': '#57a87b', // RGB: (87, 168, 123)
+    'leaf-green-40': '#9fcdb4', // RGB: (159, 205, 180)
+    'leaf-green-10': '#e7f3ec', // RGB: (231, 243, 236)
+  }
 };
