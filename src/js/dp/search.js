9ecfa140116bb3ae53f8d1ac0bb52a2e22aec4c6
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2022-01-18 10:56:42 +0000

    code cleanup

--- src/js/dp/search.js
+++ src/js/dp/search.js
@@ -1,9 +1,11 @@
-const categories = document.querySelectorAll(".category-filter > input");
-categories.forEach((item) => {
-  item.addEventListener("click", (x) =>
-    console.log(x.target.attributes.categorychildren.value)
-  );
-});
+// WIP
+//
+// const categories = document.querySelectorAll(".category-filter > input");
+// categories.forEach((item) => {
+//   item.addEventListener("click", (x) =>
+//     console.log(x.target.attributes.categorychildren.value)
+//   );
+// });
 
 /* Replace HTML of a given element with a new set from the server */
 const fetchAndReplaceHtml = (url, selector, newSelector) => {
@@ -28,29 +30,25 @@ const fetchAndReplaceHtml = (url, selector, newSelector) => {
     });
 };
 
-
-/* Get search params and strigify them.  When removing a filter this allows us to 
+/* Get search params and strigify them.  When removing a filter this allows us to
  * search for the entire filter string and remove it.
  *
- * I attemted this with urlSearchParams.delete(), however, as the filter param is 
+ * I attemted this with urlSearchParams.delete(), however, as the filter param is
  * repeated multiple times in the search params query string it is difficult to iterate and
  * extract a specific filter param.
- * 
+ *
  * Also, although it is possible to polyfill urlSearchParams, it is not supported by IE!
- */  
+ */
 const filters = document.querySelectorAll(".child-filter input");
-let strParams = window.location.search
+let strParams = window.location.search;
 filters.forEach((item) => {
-
   item.addEventListener("click", async (e) => {
-
     if (e.target.checked) {
       strParams += `&filter=${e.target.value}`;
     } else {
       strParams = strParams.replace(`&filter=${e.target.value}`, "");
     }
-    
-    console.log('strParams', strParams)
+
     await fetchAndReplaceHtml(
       `/search${strParams}`,
       ".search__results",
