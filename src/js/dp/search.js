24357538c054e0268871189379baf779e1ad3318
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2022-01-18 14:50:15 +0000

    PR fix

--- src/js/dp/search.js
+++ src/js/dp/search.js
@@ -7,51 +7,40 @@
 //   );
 // });
 
-/* Replace HTML of a given element with a new set from the server */
-const fetchAndReplaceHtml = (url, selector, newSelector) => {
-  return fetch(url, {
+const fetchHtml = async (url) => {
+  const response = await fetch(url, {
     method: "get",
     mode: "cors",
     headers: new Headers({
       Accept: "application/json",
     }),
-  })
-    .then((response) => {
-      return response.text();
-    })
-    .then((html) => {
-      const parser = new DOMParser();
-      const element = parser.parseFromString(html, "text/html");
-
-      document
-        .querySelector(selector)
-        .replaceWith(element.querySelector(newSelector));
-    });
+  });
+  return (await response) && response.text();
 };
 
-/* Get search params and strigify them.  When removing a filter this allows us to
- * search for the entire filter string and remove it.
- *
- * I attemted this with urlSearchParams.delete(), however, as the filter param is
- * repeated multiple times in the search params query string it is difficult to iterate and
- * extract a specific filter param.
- *
- * Also, although it is possible to polyfill urlSearchParams, it is not supported by IE!
- */
 const filters = document.querySelectorAll(".child-filter input");
 let strParams = window.location.search;
+
 filters.forEach((item) => {
   item.addEventListener("click", async (e) => {
     if (e.target.checked) {
       strParams += `&filter=${e.target.value}`;
     } else {
-      strParams = strParams.replace(`&filter=${e.target.value}`, "");
+      strParams = strParams.replace(`${new RegExp(/(\&|\?)filter)/)}=${e.target.value}`, "");
     }
 
-    await fetchAndReplaceHtml(
-      `/search${strParams}`,
-      ".search__results",
-      ".search__results"
-    );
+    const responseText = await fetchHtml(`/search${strParams}&page=1`);
+    if (responseText) {
+      const dom = new DOMParser().parseFromString(responseText, "text/html");
+
+      document
+        .querySelector(".search__results")
+        .replaceWith(dom.querySelector(".search__results"));
+
+      // WIP
+      // document
+      //   .querySelector(".search__pagintion")
+      //   .replaceWith(dom.querySelector(".search__pagintion"));
+    }
   });
 });
