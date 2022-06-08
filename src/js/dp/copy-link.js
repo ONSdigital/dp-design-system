b37adbc3336235966b58a27c3ca08fa9859409f9
NickL <19624419+mr-nick17@users.noreply.github.com>
2022-06-08 14:59:46 +0100

    5695 styling and interaction for copy link component

---
+++ src/js/dp/copy-link.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const copyBtn = document.querySelector(".ons-copy-link__btn");
+  const btnText = copyBtn?.querySelector(".ons-btn__inner");
+  const elemInitialContent = btnText?.textContent;
+  let timeoutId;
+
+  if (copyBtn && navigator.clipboard) {
+    copyBtn.addEventListener("click", copyText);
+    copyBtn.classList.remove("ons-u-d-no");
+  }
+
+  function copyText() {
+    clearTimeout(timeoutId);
+    navigator.clipboard
+      .writeText(window.location.href)
+      .then(success(btnText), failure(btnText))
+      .catch((error) => console.error(error));
+    timeoutId = setTimeout(resetBtnState, 5000, btnText, elemInitialContent);
+  }
+
+  function failure(elem) {
+    return function () {
+      elem.textContent = elem.dataset.copyLinkFailure;
+      copyBtn.setAttribute("aria-live", "polite");
+      copyBtn.setAttribute("disabled", "true");
+    };
+  }
+
+  function success(elem) {
+    return function () {
+      elem.textContent = elem.dataset.copyLinkSuccess;
+      copyBtn.setAttribute("aria-live", "polite");
+    };
+  }
+
+  function resetBtnState(elem, initialContent) {
+    elem.textContent = initialContent;
+    copyBtn.removeAttribute("aria-live", "polite");
+  }
+});
