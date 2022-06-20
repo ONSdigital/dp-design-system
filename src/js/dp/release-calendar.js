4ed0fc5fc7d6fe68490f9f8debf889c0517812fd
Yomi Odeleye <odeleye.yomi@me.com>
2022-06-20 06:40:40 +0100

    adding release page tracking on page load

--- src/js/dp/release-calendar.js
+++ src/js/dp/release-calendar.js
@@ -1,6 +1,7 @@
 import { findNode, gtmDataLayerPush, days } from "../utilities";
 
 const releaseCalendarContainer = document.querySelector(".release-calendar");
+const releasePageContainer = document.querySelector(".release");
 
 document.addEventListener("DOMContentLoaded", function () {
   function releaseTypeAutoSubmit(formSelector) {
@@ -46,54 +47,103 @@ if (sortSelector) {
   });
 }
 
-[
-  ...releaseCalendarContainer.querySelectorAll(
-    ".ons-radio__input[type=radio]:not(input:disabled)"
-  ),
-].map((topFilter) => {
-  topFilter.addEventListener("change", async (e) => {
-    gtmDataLayerPush({
-      event: "Filter",
-      "filter-by": e.target.name,
-      selected: e.target.value.replace("type-", ""),
+if (releaseCalendarContainer) {
+  [
+    ...releaseCalendarContainer.querySelectorAll(
+      ".ons-radio__input[type=radio]:not(input:disabled)"
+    ),
+  ].map((topFilter) => {
+    topFilter.addEventListener("change", async (e) => {
+      gtmDataLayerPush({
+        event: "Filter",
+        "filter-by": e.target.name,
+        selected: e.target.value.replace("type-", ""),
+      });
     });
   });
-});
 
-document.querySelector('[aria-label="Keywords"]').onsubmit = async (e) => {
-  const formData = new FormData(e.target);
-  const formProps = Object.fromEntries(formData);
-  gtmDataLayerPush({
-    event: "Filter",
-    "filter-by": "search",
-    "search-term": formProps.keywords,
-  });
-};
+  document.querySelector('[aria-label="Keywords"]').onsubmit = async (e) => {
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+    gtmDataLayerPush({
+      event: "Filter",
+      "filter-by": "search",
+      "search-term": formProps.keywords,
+    });
+  };
 
-document.querySelector('[aria-label="Released date"]').onsubmit = async (e) => {
-  const formData = new FormData(e.target);
-  const formProps = Object.fromEntries(formData);
-  const startDate = `${formProps["after-year"]}/${formProps["after-month"]}/${formProps["after-day"]}`;
-  const endDate = `${formProps["before-year"]}/${formProps["before-month"]}/${formProps["before-day"]}`;
+  document.querySelector('[aria-label="Released date"]').onsubmit = async (
+    e
+  ) => {
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+    const startDate = `${formProps["after-year"]}/${formProps["after-month"]}/${formProps["after-day"]}`;
+    const endDate = `${formProps["before-year"]}/${formProps["before-month"]}/${formProps["before-day"]}`;
 
-  const numberOfDays = days(
-    new Date(
-      formProps["after-year"],
-      formProps["after-month"],
-      formProps["after-day"]
-    ),
-    new Date(
-      formProps["before-year"],
-      formProps["before-month"],
-      formProps["before-day"]
-    )
+    const numberOfDays = days(
+      new Date(
+        formProps["after-year"],
+        formProps["after-month"],
+        formProps["after-day"]
+      ),
+      new Date(
+        formProps["before-year"],
+        formProps["before-month"],
+        formProps["before-day"]
+      )
+    );
+
+    gtmDataLayerPush({
+      event: "Filter",
+      "filter-by": "date-range",
+      "start-date": startDate,
+      "end-date": endDate,
+      "number-of-days": numberOfDays,
+    });
+  };
+}
+
+if (releasePageContainer) {
+  const releaseStatus = document.querySelector(".release-status").textContent;
+  const releaseDate = new Date(
+    document.querySelector(".release-date").textContent
   );
+  const releaseTime = new Date(
+    document.querySelector(".release-time").textContent
+  );
+  const releaseDateStatus = document.querySelector(
+    ".release-date-status"
+  ).textContent;
+  const nextReleaseDate = new Date(
+    document.querySelector(".next-release-date").textContent
+  );
+  const contactName = document.querySelector(".contact-name").textContent;
+
+  const year = releaseDate.getFullYear().toString().padStart(2, "0");
+  const month = releaseDate.getMonth().toString().padStart(2, "0");
+  const date = releaseDate.getDate().toString().padStart(2, "0");
+  const hour = releaseTime.getHours().toString().padStart(2, "0");
+  const minutes = releaseTime.getMinutes().toString().padStart(2, "0");
+
+  const nextReleaseYear = nextReleaseDate
+    .getFullYear()
+    .toString()
+    .padStart(2, "0");
+  const nextReleaseMonth = nextReleaseDate
+    .getMonth()
+    .toString()
+    .padStart(2, "0");
+  const nextReleaseDayDate = nextReleaseDate
+    .getDate()
+    .toString()
+    .padStart(2, "0");
 
   gtmDataLayerPush({
-    event: "Filter",
-    "filter-by": "date-range",
-    "start-date": startDate,
-    "end-date": endDate,
-    "number-of-days": numberOfDays,
+    "release-status": releaseStatus,
+    "release-date": `${year}${month}${date}`,
+    "release-time": `${hour}:${minutes}`,
+    "release-date-status": releaseDateStatus,
+    "contact-name": contactName,
+    "next-release-date": `${nextReleaseYear}${nextReleaseMonth}${nextReleaseDayDate}`,
   });
-};
+}
