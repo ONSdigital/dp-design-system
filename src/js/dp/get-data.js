6367bf41565efaccc15b9edf8240a8c0d2f1bb1d
Tom Ridd <twridd@gmail.com>
2023-03-27 10:32:24 +0100

    add param to spinner call to allow us to return only appropriate data

--- src/js/dp/get-data.js
+++ src/js/dp/get-data.js
@@ -3,7 +3,7 @@ import { fetchHtml, replaceWithIEPollyfill, gtmDataLayerPush } from "../utilitie
 document.addEventListener("DOMContentLoaded", function () {
   // Checking for downloads with loading spinner
   const getPage = async () => {
-    const responseText = await fetchHtml(window.location.pathname);
+    const responseText = await fetchHtml(window.location.pathname + "?spinner=true");
     const dom = new DOMParser().parseFromString(responseText, "text/html");
     return dom.querySelector("[data-get-data-form-downloads=ready]");
   }
