7f5628d566905b44bbaadfda5a7c7bbfd7a63505
Joshua Singh <joshua.singh@ons.gov.uk>
2023-06-15 09:53:44 +0100

    Added cookies-pref to design system. Imported cookies-pref to main.

---
+++ src/js/dp/cookies-pref.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    if (document.querySelectorAll('.cookies-no-js').length > 0) {
+        document.querySelectorAll('.cookies-no-js').forEach((item) => item.classList.add('hidden'))
+    }
+    if (document.querySelectorAll('.cookies-js').length > 0) {
+        document.querySelectorAll('.cookies-js').forEach((item) => item.classList.remove('hidden'))
+    }
+});
