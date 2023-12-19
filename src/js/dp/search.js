3f73778b4e75663ad26a83928450d01651f8b320
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2023-12-19 09:58:56 +0000

    1949 guard clause fixes

--- src/js/dp/search.js
+++ src/js/dp/search.js
@@ -179,10 +179,10 @@ if (searchContainer) {
             tmpValue.push(param.topics);
             url.searchParams.append(strParamType, tmpValue);
           }
-        } else if (tmpValues.length <= 1) {
-          const tmpValue = tmpValues[0].split(',');
-          const tmpParam = tmpValue.filter((e) => e !== param.topics);
-          if (tmpParam.length !== 0) {
+        } else if (tmpValues.length <= 1 && tmpValues.length !== 0) {
+          const tmpValue = tmpValues[0]?.split(',');
+          const tmpParam = tmpValue?.filter((e) => e !== param.topics);
+          if (tmpParam?.length !== 0) {
             url.searchParams.append(strParamType, tmpParam);
           }
         }
