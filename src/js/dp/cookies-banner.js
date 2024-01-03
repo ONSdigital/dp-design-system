3949b293bf1b7ec19a10b5695d68af9fd5bc34a2
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2024-01-03 14:57:44 +0000

    1871 move function definition before function call

--- src/js/dp/cookies-banner.js
+++ src/js/dp/cookies-banner.js
@@ -1,34 +1,43 @@
 // cookies settings
+function hasCookiesPreferencesSet() {
+  return document.cookie.indexOf('cookies_preferences_set=true') > -1;
+}
+
+function userIsOnCookiesPreferencesPage() {
+  const href = window.location.href.split('/');
+
+  // check that last element in href array is 'cookies' - in case we add further pages
+  // within the cookies path
+  const isCookiesPreferencesPage = href[href.length - 1] === 'cookies';
+  return isCookiesPreferencesPage;
+}
+
+function extractDomainFromUrl(url) {
+  if (url.indexOf('localhost') >= 0 || url.indexOf('127.0.0.1') >= 0) {
+    return 'localhost';
+  }
+
+  // top level domains (TLD/SLD) in use
+  const pattern = '(.uk|.onsdigital.uk|.gov.uk)';
+  const tlds = new RegExp(pattern);
+
+  const topLevelDomain = url.match(tlds)[0];
+  const secondLevelDomain = url.replace(topLevelDomain, '').split('.').pop();
+
+  return `.${secondLevelDomain}${topLevelDomain}`;
+}
+
 const cookiesSet = hasCookiesPreferencesSet();
-const cookiesBanner = document.querySelector('.js-cookies-banner-form');
-
-const oneYearInSeconds = 31622400;
-const currentUrl = window.location.hostname;
-const cookiesDomain = extractDomainFromUrl(currentUrl);
-const cookiesPreference = true;
-const encodedCookiesPolicy = '%7B%22essential%22%3Atrue%2C%22usage%22%3Atrue%7D';
-const cookiesPath = '/';
-
-function determineWhetherToRenderBanner() {
-  if (!cookiesSet && !userIsOnCookiesPreferencesPage()) {
-    cookiesBanner.classList.remove('cookies-banner--hidden');
-    initCookiesBanner();
-  }
-}
-
-function initCookiesBanner() {
-  const jsHideBanner = document.querySelector('.js-hide-cookies-banner');
-  if (jsHideBanner) {
-    jsHideBanner.addEventListener('click', () => cookiesBanner.classList.add('hidden'));
-  }
-
-  if (cookiesBanner) {
-    cookiesBanner.addEventListener('submit', submitCookieForm);
-  }
-}
 
 function submitCookieForm(e) {
   e.preventDefault();
+
+  const oneYearInSeconds = 31622400;
+  const currentUrl = window.location.hostname;
+  const cookiesDomain = extractDomainFromUrl(currentUrl);
+  const cookiesPreference = true;
+  const encodedCookiesPolicy = '%7B%22essential%22%3Atrue%2C%22usage%22%3Atrue%7D';
+  const cookiesPath = '/';
   const cookiesAcceptBanner = document.querySelector('.js-accept-cookies');
 
   if (cookiesAcceptBanner) {
@@ -52,31 +61,23 @@ function submitCookieForm(e) {
   }
 }
 
-function extractDomainFromUrl(url) {
-  if (url.indexOf('localhost') >= 0 || url.indexOf('127.0.0.1') >= 0) {
-    return 'localhost';
+const cookiesBanner = document.querySelector('.js-cookies-banner-form');
+function initCookiesBanner() {
+  const jsHideBanner = document.querySelector('.js-hide-cookies-banner');
+  if (jsHideBanner) {
+    jsHideBanner.addEventListener('click', () => cookiesBanner.classList.add('hidden'));
   }
 
-  // top level domains (TLD/SLD) in use
-  const tlds = new RegExp('(.uk|.onsdigital.uk|.gov.uk)');
-
-  const topLevelDomain = url.match(tlds)[0];
-  const secondLevelDomain = url.replace(topLevelDomain, '').split('.').pop();
-
-  return `.${secondLevelDomain}${topLevelDomain}`;
+  if (cookiesBanner) {
+    cookiesBanner.addEventListener('submit', submitCookieForm);
+  }
 }
 
-function hasCookiesPreferencesSet() {
-  return document.cookie.indexOf('cookies_preferences_set=true') > -1;
-}
-
-function userIsOnCookiesPreferencesPage() {
-  const href = window.location.href.split('/');
-
-  // check that last element in href array is 'cookies' - in case we add further pages
-  // within the cookies path
-  const isCookiesPreferencesPage = href[href.length - 1] === 'cookies';
-  return isCookiesPreferencesPage;
+function determineWhetherToRenderBanner() {
+  if (!cookiesSet && !userIsOnCookiesPreferencesPage()) {
+    cookiesBanner.classList.remove('cookies-banner--hidden');
+    initCookiesBanner();
+  }
 }
 
 document.addEventListener('DOMContentLoaded', determineWhetherToRenderBanner);
