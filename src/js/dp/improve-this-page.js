c8c16586c7753c068546155c81628c426fa2eac9
Ighomena Odebala <ighomena.odebala@ons.gov.uk>
2025-03-04 09:01:23 +0000

    Remove check for feedback api

--- src/js/dp/improve-this-page.js
+++ src/js/dp/improve-this-page.js
@@ -5,21 +5,17 @@ import {
 } from './validation/validation';
 
 function fetchFeedbackAPI(
-  useUrlEncoding,
   url,
   feedbackFormHeader,
   form,
   feedbackMessageError,
   feedbackMessage,
 ) {
-  const contentType = useUrlEncoding
-    ? 'application/x-www-form-urlencoded'
-    : 'application/json; charset=UTF-8';
   const fetchConfig = {
     method: 'POST',
     body: form,
     headers: new Headers({
-      'Content-Type': contentType,
+      'Content-Type': 'application/json; charset=UTF-8',
     }),
   };
   const formHeader = feedbackFormHeader;
@@ -38,22 +34,13 @@ function fetchFeedbackAPI(
     });
 }
 
-function serializeFormData(form) {
-  const data = new FormData(form);
-  const serializedData = new URLSearchParams(data).toString();
-  return serializedData;
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   const pageURL = window.location.href;
-  const feedbackPath = '/feedback';
-  const positiveFeedbackPath = `${feedbackPath}/thanks`;
   const feedbackFormHeader = document.querySelector('#feedback-form-header');
   const feedbackMessage = '<span id="feedback-form-confirmation">Thank you. Your feedback will help us as we continue to improve the service.</span>';
   const feedbackMessageError = '<span id="feedback-form-error role="alert">Something went wrong, try using our <a href="/feedback">feedback form</a>.</span>';
   const { title } = document;
 
-  const useFeedbackAPI = document.querySelector('#feedback-api-enabled');
   const feedbackAPIURL = document.querySelector('#feedback-api-url');
 
   const feedbackFormURL = document.querySelector('#feedback-form-url');
@@ -88,35 +75,20 @@ document.addEventListener('DOMContentLoaded', () => {
   if (feedbackFormYes && feedbackFormHeader) {
     feedbackFormYes.addEventListener('click', (e) => {
       e.preventDefault();
-      const feedbackFormContainer = document.querySelector(
-        '#feedback-form-container',
-      );
 
-      if (useFeedbackAPI && useFeedbackAPI.value === 'true' && feedbackAPIURL) {
-        const postObject = {
-          is_page_useful: true,
-          is_general_feedback: false,
-          ons_url: pageURL,
-        };
-        const postJson = JSON.stringify(postObject);
-        fetchFeedbackAPI(
-          false,
-          feedbackAPIURL.value,
-          feedbackFormHeader,
-          postJson,
-          feedbackMessageError,
-          feedbackMessage,
-        );
-      } else {
-        fetchFeedbackAPI(
-          true,
-          positiveFeedbackPath,
-          feedbackFormHeader,
-          serializeFormData(feedbackFormContainer),
-          feedbackMessageError,
-          feedbackMessage,
-        );
-      }
+      const postObject = {
+        is_page_useful: true,
+        is_general_feedback: false,
+        ons_url: pageURL,
+      };
+      const postJson = JSON.stringify(postObject);
+      fetchFeedbackAPI(
+        feedbackAPIURL.value,
+        feedbackFormHeader,
+        postJson,
+        feedbackMessageError,
+        feedbackMessage,
+      );
     });
   }
 
@@ -151,34 +123,23 @@ document.addEventListener('DOMContentLoaded', () => {
         return;
       }
 
-      if (useFeedbackAPI && useFeedbackAPI.value === 'true' && feedbackAPIURL) {
-        const postObject = {
-          is_page_useful: false,
-          is_general_feedback: false,
-          feedback: descriptionField.value,
-          ons_url: pageURL,
-          name: nameField.value,
-          email_address: emailField.value,
-        };
-        const postJson = JSON.stringify(postObject);
-        fetchFeedbackAPI(
-          false,
-          feedbackAPIURL.value,
-          feedbackFormHeader,
-          postJson,
-          feedbackMessageError,
-          feedbackMessage,
-        );
-      } else {
-        fetchFeedbackAPI(
-          true,
-          feedbackPath,
-          feedbackFormHeader,
-          serializeFormData(feedbackFormContainer),
-          feedbackMessageError,
-          feedbackMessage,
-        );
-      }
+      const postObject = {
+        is_page_useful: false,
+        is_general_feedback: false,
+        feedback: descriptionField.value,
+        ons_url: pageURL,
+        name: nameField.value,
+        email_address: emailField.value,
+      };
+      const postJson = JSON.stringify(postObject);
+      fetchFeedbackAPI(
+        feedbackAPIURL.value,
+        feedbackFormHeader,
+        postJson,
+        feedbackMessageError,
+        feedbackMessage,
+      );
+
       feedbackFormHeader.classList.toggle('js-hidden');
       feedbackForm.classList.toggle('js-hidden');
     });
